f629ed74fe99149c10ebffe4afa6a377af35b1ba
bijoy7034 <bijoyanil74@gmail.com>
2025-08-12 21:06:56 +0530

    feat: implement authentication page with login form and styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Auth from './pages/auth'
 
 function App() {
   return (
     <>
-        Hello
+    <Auth/>
     </>
   )
 }
--- client/src/pages/auth.tsx
+++ client/src/pages/auth.tsx
@@ -1,9 +1,47 @@
-function auth() {
+function Auth() {
   return (
-    <div>
-        <h1>Authentication Page</h1>
+    <div
+      className="container login"
+      style={{ width: "500px", padding: "40px" }}
+    >
+      <b>
+        <h3 className="text-center mb-4 text-bold">Login</h3>
+      </b>
+
+      {/* {error && (
+        <div className="alert alert-danger" role="alert">
+          {error}
+        </div>
+      )} */}
+
+      <form>
+        <div className="mb-3">
+          <label className="form-label">Email address</label>
+          <input type="email" className="form-control form-control-sm" required />
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label">Password</label>
+          <input type="password" className="form-control form-control-sm" required />
+        </div>
+
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label">Save Login Info</label>
+        </div>
+
+        <div className="d-grid gap-2 mb-4 mt-2">
+          <button className="btn btn-dark" type="submit">
+            Login
+          </button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default auth
+export default Auth;
